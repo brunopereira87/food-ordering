7d8abb830506eda97f29825c9d2cf87ff96ea30c
Bruno Emanuel Pereira <bruno_emanuel87@hotmail.com>
2024-06-19 09:41:47 -0300

    ProductListItem component created

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,31 +1,18 @@
-import { StyleSheet } from 'react-native';
+import { View } from 'react-native';
+import { ProductListItem } from '@/src/components/ProductListItem';
 
-import EditScreenInfo from '@/src/components/EditScreenInfo';
-import { Text, View } from '@/src/components/Themed';
+import products from '@/assets/data/products';
 
-export default function TabOneScreen() {
+
+
+export default function MenuScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One Jabulani Milani Senju</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+    <View>
+      { products.map(product => (
+        <ProductListItem key={product.id} product={product} />
+      ))}
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
+
